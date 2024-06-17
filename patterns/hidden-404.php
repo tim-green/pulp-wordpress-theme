339e83b75eff75a3d7c7e4f9a2190d203beb70d5
Tim <github@timgreen.ws>
2024-06-17 11:23:28 +1000

    added hidden based patterns

---
+++ patterns/hidden-404.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: 404 page
+ * Slug: pulp/404
+ * Inserter: no
+ */
+?>
+<!-- wp:heading {"level":1} -->
+	<h1><?php echo esc_html__( 'Not found, error 404', 'pulp' ); ?></h1>
+<!-- /wp:heading -->
+<!-- wp:paragraph -->
+	<p><?php echo esc_html__( 'Oops, the page you are looking for does not exist or is no longer available. Please use the search form below to find your way.', 'pulp' ); ?></p>
+<!-- /wp:paragraph -->
+<!-- wp:search {"width":80,"widthUnit":"%","showLabel":false,"buttonText":"Search"} /-->
---
+++ patterns/hidden-post-terms.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Post terms
+ * Slug: pulp/post-terms
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"tagName":"footer","style":{"spacing":{"blockGap":"0px"}},"className":"entry-footer","layout":{"type":"constrained"}} -->
+<footer class="wp-block-group entry-footer">
+	<!-- wp:post-terms {"term":"category","prefix":"<?php echo esc_html__( 'In:', 'pulp' ); ?> "} /-->
+	<!-- wp:post-terms {"term":"post_tag","prefix":"<?php echo esc_html__( 'Tags:', 'pulp' ); ?> "} /-->
+</footer>
+<!-- /wp:group -->
